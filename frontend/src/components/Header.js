dbf48e8587e1ce12093a4175f541cba9fe95e401
Wellington Roth <wellington_roth@hotmail.com>
2020-05-31 12:14:06 -0300

    feed, likes e new post

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import'./Header.css';
+import logo from "../assets/logo.svg";
+import camera from "../assets/camera.svg";
 
 export default function Header() {
   return (
     <header id="main-header">
       <div className="header-content">
-        <img src="" alt="WellGram" />
-        <img src="" alt="Enviar publicação" />
+        <Link to="/">
+          <img src={logo} alt="WellGram" />
+        </Link>
+        <Link to="/new">
+          <img src={camera} alt="Enviar publicação" />
+        </Link>
       </div>
     </header>
   );
